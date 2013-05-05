bfbb9083afd76facff182d1c4af66dc85fedfb42
Colin Fitzsimons <colin.altered@gmail.com>
2013-05-05 18:51:56 +0100

    Buffer stub upload

---
+++ BuffyTheVampireSlayer.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.net.Socket;
+
+class BuffyTheVampireSlayer{
+    private ArrayList<Socket> socketList;
+    private ArrayList<String> messageList;
+
+    public BuffyTheVampireSlayer(Socket startingSocket){
+        
+    }
+    public synchronized void insert(String message){
+    
+    }
+    public synchronized String remove(){
+    
+    }
+    public void addSocket(Socket toAdd){
+    
+    
+    }
+    public void removeSocket(int location){
+    
+    }
+    
+}
